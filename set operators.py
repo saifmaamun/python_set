5d66828c458ebf11d7f1b51a52ac3dca89ebb266
Saif Maamun <sadifmamun@gmail.com>
2024-11-29 08:44:49 +0600

    some basic set operators

---
+++ set operators.py
@@ -0,0 +1,17 @@
+ # union is addresed by | , and returns a new set
+A={1,2,3,6,59,7}
+B={5,2,3,6,9,7,2,0}
+C= A|B
+print(C)
+
+# intersection
+D= A&B
+print(D)
+
+# difference
+E= A-B
+print(E)
+
+# symetric difference
+F=A^B
+print(F)
--- sets.py
+++ sets.py
@@ -1,3 +1,7 @@
 #sets are unordered, no duplicates
 set1={10,50,20,30,9,8}
 print(set1)
+
+set2=set((2,1,5,4,6,9,8,4,3,6,4))
+print(set2)
+
